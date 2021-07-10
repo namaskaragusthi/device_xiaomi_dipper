6056a96e12738397bf1cb9477a6dec80f6497169
namaskaragusthi <senaoutsider123@gmail.com>
2021-07-10 10:29:26 +0000

    dipper: update fingerprint to Redfin July

--- nad_dipper.mk
+++ nad_dipper.mk
@@ -21,10 +21,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.005/7349499:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.005 7349499 release-keys" \
     PRODUCT_NAME="dipper"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
