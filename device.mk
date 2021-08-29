0ccbb392a33d0d5f51bc8433fc42275a867ac5d9
namaskaragusthi <senaoutsider123@gmail.com>
2021-08-29 13:19:19 +0000

    dipper: Build MiuiCamera

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/dipper/dipper-vendor.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2248
 TARGET_SCREEN_WIDTH := 1080
